96a8e81f1ef52894ca5a805f8e93450d9249f513
Ri-tesh <rritesh.ppradhan@gmail.com>
2022-11-23 02:46:30 -0600

    first commit

---
+++ src/day2/Plant.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class Plant {
+    // States
+    // Common Name
+    // Species
+    // Genus
+    // Types
+    // Color
+    // Size
+    // Temperature Requirement
+    // Sunlight Requirement
+    // Plant Categories
+
+
+
+    //Behaviours
+}
